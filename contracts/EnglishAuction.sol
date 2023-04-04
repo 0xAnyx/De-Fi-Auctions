bff6b1cff38f19b6ac42548beb2db70b6b98eb1f
Anyx <itsmenick30@gmail.com>
2023-04-04 09:50:09 +0530

    English auction contract updated

--- contracts/EnglishAuction.sol
+++ contracts/EnglishAuction.sol
@@ -5,69 +5,81 @@ import { IERC721 } from '@openzeppelin/contracts/token/ERC721/IERC721.sol';
 
 contract EngAuction {
 
-    event tokenListed(address nft, uint nftId, address seller, uint startingPrice);
-    event successfullyBid(address nft, uint nftId, address indexed bidder, uint bidAmount);
-    event auctionEnded(address nft, uint nftId, address buyer, uint sellingPrice);
+    uint public totalAuctionIds;
 
-    struct itemDetails {
+    event tokenListed(address _nft, uint _nftId, address seller, uint startingPrice);
+    event successfullyBid(uint auctionId, address indexed bidder, uint bidAmount);
+    event successfullyWithdrawn(uint auctionId, address indexed withdrawer, uint amount);
+    event auctionEnded(uint auctionId, address buyer, uint sellingPrice);
+
+    struct bidDetails {
         address seller;
         bool isListed;
         address highestBidder;
         uint highestBid;
         uint duration;
+        address nftAddress;
+        uint nftId;
     }
 
-    mapping (address => mapping (uint => itemDetails)) public aucDetails;
-//    mapping (address => mapping (uint => mapping (address => uint))) public bids;
+    mapping (uint => bidDetails) public auctionDetails;
+    mapping (bytes => uint) public getAuctionId;
+    mapping (address => mapping(uint => uint)) public bids;
 
     function listNFT(address _nft, uint _nftId, uint _startingPrice) external {
         require(_nft != address(0), 'Invalid Nft Address');
         require(_startingPrice != 0, 'Base Price cannot be zero');
         IERC721 nft = IERC721(_nft);
         require(nft.ownerOf(_nftId) == msg.sender, 'Caller not owner');
-        aucDetails[_nft][_nftId] = itemDetails(msg.sender, true, address(0), _startingPrice, 1 days);
+        getAuctionId[convertNftDetailsToBytes(_nft,_nftId)] = totalAuctionIds + 1;
+        auctionDetails[totalAuctionIds + 1] = bidDetails(msg.sender, true, address (0),
+            _startingPrice, 1 days, _nft, _nftId);
+        totalAuctionIds += 1;
         nft.transferFrom(msg.sender, address(this), _nftId);
         emit tokenListed(_nft, _nftId, msg.sender, _startingPrice);
     }
 
-    function bid(address _nft, uint _nftId) external payable {
-        require(_nft != address(0), 'Invalid Nft Address');
-        itemDetails storage token = aucDetails[_nft][_nftId];
+    // @notice Add the incremental bid on top of the highest bid and not the sum total
+    function bid(uint _auctionId) external payable {
+        bidDetails storage token = auctionDetails[_auctionId];
         require(token.isListed, 'Token not listed');
+        require(msg.value != 0, 'Cannot be 0');
         require(msg.value > token.highestBid, 'Bid more than highest bid');
-        require(block.timestamp <= token.duration, 'Bidding time over');
+        require(block.timestamp <= block.timestamp + token.duration, 'Bidding time over');
         require(token.highestBidder != msg.sender, "Can't raise self bid");
-        uint currentBid = aucDetails[_nft][_nftId].highestBid;
-        uint currentBidder = aucDetails[_nft][_nftId].highestBidder;
-        aucDetails[_nft][_nftId].highestBid = msg.value;
-        aucDetails[_nft][_nftId].highestBid = msg.sender;
-//        bids[_nft][_nftId][msg.sender] += msg.value;
-        if (currentBidder != address(0)) {
-            (bool success, bytes memory data) = currentBidder.call{value: currentBid}("");
-            require(success, 'Failed to transfer Ether');
-        }
-        emit successfullyBid(_nft, _nftId, msg.sender, msg.value);
+        auctionDetails[_auctionId].highestBid += msg.value;
+        auctionDetails[_auctionId].highestBidder = msg.sender;
+        bids[msg.sender][_auctionId] += msg.value;
+        emit successfullyBid(_auctionId, msg.sender, msg.value);
     }
 
-//    function withdrawOnGettingOutbidden(address _nft, uint _nftId) external {
-//        require(_nft != address(0), 'Invalid Nft Address');
-//        itemDetails storage token = aucDetails[_nft][_nftId];
-//    }
-    function endAuction(address _nft, uint _nftId) external {
-        require(_nft != address(0), 'Invalid Nft Address');
-        itemDetails storage token = aucDetails[_nft][_nftId];
+    function withdrawBidOnGettingOutbidden(uint _auctionId) external {
+        bidDetails storage token = auctionDetails[_auctionId];
         require(token.isListed, 'Token not listed');
-        require(block.timestamp > token.duration, 'Auction in progress');
+        require(msg.sender != token.highestBidder, "Highest bidder can't withdraw bid");
+        uint bidAmount = bids[msg.sender][_auctionId];
+        bids[msg.sender][_auctionId] = 0;
+        payable(msg.sender).transfer(bidAmount);
+        emit successfullyWithdrawn(_auctionId, msg.sender, bidAmount);
+    }
 
-        address memory highestBidder = token.highestBidder;
-
+    function endAuction(uint _auctionId) external {
+        bidDetails storage token = auctionDetails[_auctionId];
+        require(token.isListed, 'Token not listed');
+        require(block.timestamp > block.timestamp + token.duration, 'Auction in progress');
+        address highestBidder = token.highestBidder;
         if(highestBidder != address (0)) {
-            IERC721(_nft).transferFrom(address(this), highestBidder, _nftId);
+            IERC721(token.nftAddress).transferFrom(address(this), highestBidder, token.nftId);
             payable(token.seller).transfer(token.highestBid);
         }
         else
-            IERC721(_nft).transferFrom(address(this), token.seller, _nftId);
-        emit auctionEnded(_nft, _nftId, highestBidder, token.highestBid);
+            IERC721(token.nftAddress).transferFrom(address(this), token.seller, token.nftId);
+        emit auctionEnded(_auctionId, highestBidder, token.highestBid);
     }
-    
+
+    function convertNftDetailsToBytes(address _nft, uint _nftId) private returns(bytes memory) {
+        bytes memory converted = abi.encodePacked(_nft,_nftId);
+        return converted;
+    }
+
 }
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,61 @@
 import { HardhatUserConfig } from "hardhat/config";
+import "hardhat-abi-exporter";
 import "@nomicfoundation/hardhat-toolbox";
-
+import "@openzeppelin/hardhat-upgrades";
+import "hardhat-deploy";
+import "hardhat-tracer";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-web3";
+import * as dotenv from "dotenv";
+import "hardhat-gas-reporter";
+dotenv.config({ path: __dirname + "/.env" });
 const config: HardhatUserConfig = {
-  solidity: "0.8.12",
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  // networks: {
+  //   goerli: {
+  //     url: "https://goerli.infura.io/v3/2fa7616a97224f70b68b56e5cd3ef2c4",
+  //     accounts: [process.env.PRIVATE_KEY || ""],
+  //   },
+  //   mainnet: {
+  //     url: "https://mainnet.infura.io/v3/2fa7616a97224f70b68b56e5cd3ef2c4",
+  //     accounts: [process.env.PRIVATE_KEY || ""],
+  //   },
+  //   polygonMainnet: {
+  //     url: "https://polygon-mainnet.g.alchemy.com/v2/MsfKKcvyX7YUvgH2jU7BQTbPzlKKgQba",
+  //     accounts: [process.env.PRIVATE_KEY || ""],
+  //   },
+  //   mumbai: {
+  //     url: "https://polygon-mumbai.infura.io/v3/6422400310bc4cb784d6a819632808b9",
+  //     accounts: [process.env.PRIVATE_KEY || ""],
+  //   },
+  // },
+
+  etherscan: {
+    apiKey: {
+      polygonMumbai: "VIT7XVFNT1RIGIMPDPY6QKEVJJ94DSNVVW",
+      polygon: "9UHP9XAJW9C5CGVRG5IQ29ZEKTB7N12TRE",
+      goerli: "31WXEYFAGW4JBBSRRJZRJQB2GB5D6MB48W",
+      mainnet: "31WXEYFAGW4JBBSRRJZRJQB2GB5D6MB48W",
+    },
+  },
+
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    outputFile: "gas-report-mint-burn.txt",
+    showTimeSpent: true,
+    noColors: true,
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+  },
+
 };
 
 export default config;
